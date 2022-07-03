799d06f10fefd602821d37e90c222008276c7c44
Mickyas Balchu <55349901+bmike0059@users.noreply.github.com>
2022-07-03 23:33:47 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,8 +7,8 @@
  */
 int is_palindrome(listint_t **head)
 {
-    //const listint_t *current;
-    //const listint_t *tail;
+    const listint_t *current;
+    const listint_t *tail;
     unsigned int n = 0;
 
     //Get length
@@ -17,5 +17,46 @@ int is_palindrome(listint_t **head)
     //return false if one check was False
     //return true at the end (if all passed the checks)
 
-    return (n);
+    current = *head;
+    if(current == NULL)
+        return (1);
+
+    tail = current;
+
+    while (tail->next != NULL)
+    {
+        tail = tail->next;
+        n++;
+    }
+
+    if ((n+1)%2 != 0)
+        return (0);
+    else
+    {
+        int arr[n+1];
+        int i=0;
+
+        while (i <= n)
+        {
+            arr[i] = current->n;
+            current = current->next;
+            i++;
+        }
+
+        int p=0;
+        int j=n;
+        while (p < j)
+        {
+            if (arr[p] == arr[j])
+            {
+                p++;
+                j--;
+                continue;
+            }
+            else
+                return (0);
+        }
+        return (1);
+    }
+
 }
